94fa415498161e658a37d32f864bdea765f8e5d5
Alvariill0 <alvarillo0412@gmail.com>
2025-06-01 20:26:08 +0200

    Ajuste rutas Api
    cambiado a ruta relativa

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
-// Obtener el nombre de la carpeta del proyecto dinámicamente
-const projectPath = path.basename(process.cwd())
+// Obtener la ruta completa del proyecto
+const fullPath = process.cwd()
+// Extraer la ruta relativa desde htdocs
+const relativePath = fullPath.split('htdocs')[1].replace(/\\/g, '/')
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: `http://localhost/${projectPath}/api`,
+        target: `http://localhost${relativePath}/api`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
